8b30d8b36f327e87b8e8d69f77dadff159d859c6
PenguinKKIM <clfrck124@gmail.com>
2023-08-21 18:48:33 +0900

    두번뜨는 문제 수정
    멍청이야

--- study/class/ClassTeat5.java
+++ study/class/ClassTeat5.java
@@ -1,86 +1,83 @@
-class Account{
+class Account {
 	String id;
 	String name;
 	int balance;
-	
-	
+
 	int deposit(int money) {
 		balance += money;
 		return balance;
 	}
-	
+
 	int withdraw(int money) {
-		if(balance>=money)balance -= money;
+		if (balance >= money)
+			balance -= money;
 		return balance;
 	}
 
 	String info() {
-		return "계좌번호 : "+ id +", "+ "이름 : " + name + ", " + "잔액 : " + balance; 
+		return "계좌번호 : " + id + ", " + "이름 : " + name + ", " + "잔액 : " + balance;
 	}
 
 }
 
 class Bank {
 	Account[] accs = new Account[100];
-	int accCnt; //생성된 계좌의 갯수를 관리
-	
+	int accCnt; // 생성된 계좌의 갯수를 관리
+
 	void makeAcount(String id, String name, int money) {
 		Account acc = new Account();
-		acc.id =id;
-		acc.name= name;
-		acc.balance=money;
+		acc.id = id;
+		acc.name = name;
+		acc.balance = money;
 		accs[accCnt++] = acc;
-	} //만들어서 accCnt 에 넣어야함
-	
-	void allAccountInfo() {
-		for(int i = 0; i < accCnt; i++) {
-			System.out.println(accs[i].info());
-		}
-	}
-	
-	void accountInfo(String id) {
-		for(int i = 0; i < accCnt; i++) {
-			if (accs[i].id == id) 
-			{System.out.println(accs[i].info());
-			}
-		}
-	}
-	
-	void withdraw (String id, int money) {
-		for(int i = 0; i < accCnt; i++) {
-			if (accs[i].id == id) {
-				accs[i].balance -= money;
-			}
-			System.out.println(accs[i].info());
-		}
-	}
-	
-	void deposit (String id, int money) {
-		for(int i = 0; i < accCnt; i++) {
-			if (accs[i].id == id)
-				accs[i].balance += money;
-			System.out.println(accs[i].info());
-		}
-	}
-	
-}
+	} // 만들어서 accCnt 에 넣어야함
 
-public class ClassTeat5 {
-	public static void main(String[] args) {
-		
-		Bank bank = new Bank();
-		bank.makeAcount("10001", "고길동", 1000000); //계좌번호, 이름, 잔액
-		bank.makeAcount("10002", "김길동", 2000000); //계좌번호, 이름, 잔액
-		
-		bank.allAccountInfo();
-		bank.accountInfo("10001");
-		
-		bank.withdraw("10001", 10000); //출금
-		bank.accountInfo("10001");
-	
-		bank.deposit("10001", 10000); //입금
-		bank.accountInfo("10001");
-		
+	void allAccountInfo() {
+		for (int i = 0; i < accCnt; i++) {
+			System.out.println(accs[i].info());
+		}
+	}
+
+	void accountInfo(String id) {
+		for (int i = 0; i < accCnt; i++) {
+			if (accs[i].id == id) {
+				System.out.println(accs[i].info());
+			}
+		}
+	}
+
+	void withdraw(String id, int money) {
+		for (int i = 0; i < accCnt; i++) {
+			if (accs[i].id == id) {
+				accs[i].balance = accs[i].balance - money;
+			}
+		}
+	}
+
+	void deposit(String id, int money) {
+		for (int i = 0; i < accCnt; i++) {
+			if (accs[i].id == id) {
+				accs[i].balance = accs[i].balance + money;
+			}
+		}
+	}
+}
+	public class ClassTeat5 {
+		public static void main(String[] args) {
+
+			Bank bank = new Bank();
+			bank.makeAcount("10001", "고길동", 1000000); // 계좌번호, 이름, 잔액
+			bank.makeAcount("10002", "김길동", 2000000); // 계좌번호, 이름, 잔액
+
+			bank.allAccountInfo();
+			System.out.println();
+			bank.accountInfo("10001");
+
+			bank.withdraw("10001", 10000); // 출금
+			bank.accountInfo("10001"); //출금한 금액이 나와야함
+			bank.deposit("10001", 50000); // 입금
+			bank.accountInfo("10001"); //입금한 금액이 나와야함
+
 //		Account acc1 = new Account();
 //		acc1.id="10001";
 //		acc1.name = "고길동";
@@ -93,6 +90,7 @@ public class ClassTeat5 {
 //		
 //		acc1.withdraw(20000); //출금
 //		System.out.println(acc1.info());
-		
+
+		}
 	}
-}
+
